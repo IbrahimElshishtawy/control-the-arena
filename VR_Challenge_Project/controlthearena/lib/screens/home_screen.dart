5f67895b87d6f52f76ca3d6fcc25d1f5c58c9251
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-23 00:18:36 +0200

    Create home_screen.dart

---
+++ VR_Challenge_Project/controlthearena/lib/screens/home_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import '../services/game_socket_service.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final GameSocketService socket = GameSocketService();
+
+  String logText = "Not Connected";
+
+  @override
+  void initState() {
+    super.initState();
+    socket.onMessage = (data) {
+      setState(() {
+        logText = "Received: ${data.toString()}";
+      });
+    };
+  }
+
+  void _connect() {
+    socket.connect();
+    socket.sendHandshake();
+    setState(() {
+      logText = "Connecting...";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Control The Arena")),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: _connect,
+              child: const Text("Connect to Python Server"),
+            ),
+            const SizedBox(height: 20),
+            Text(logText, style: const TextStyle(fontSize: 16)),
+          ],
+        ),
+      ),
+    );
+  }
+}
